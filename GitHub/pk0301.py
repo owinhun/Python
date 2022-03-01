85fc1bf1f93dc060bffba513661802bbe546e124
owinhun <gnsgus190@naver.com>
2022-03-01 15:38:21 +0900

    2581

--- GitHub/pk0301.py
+++ GitHub/pk0301.py
@@ -1,1 +1,23 @@
-print('Hello world')
+# print('Hello world')
+
+M = int(input())
+N = int(input())
+
+l1 = []
+
+for i in range(M,N+1):
+    er = 0
+    if i > 1:
+        for j in range(2,i):
+            if i % j == 0:
+                er += 1
+                break
+        if er == 0:
+            l1.append(i)
+
+if len(l1) > 0:
+    print(sum(l1))           
+    print(min(l1))
+
+else:
+    print(-1)
