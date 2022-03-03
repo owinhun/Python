9c95105500fc31506f2599ff0e4e3e2940150e3a
owinhun <gnsgus190@naver.com>
2022-03-03 19:28:53 +0900

    pk0303

---
+++ GitHub/pk0303.py
@@ -0,0 +1,65 @@
+# 11022
+# T = int(input())
+
+# for i in range(T):
+#     a, b = map(int, input().split())
+#     c = a + b
+
+#     print("Case #%s: %s + %s = %s"%(i + 1, a, b, c))
+
+# 2741
+# N = int(input())
+
+# for i in range(N):
+#     print(i + 1)
+
+# 10988
+
+# word = list(input())
+
+# if list(reversed(word)) == word:
+#     print(1)
+
+# else:
+#     print(0)
+
+# 4101
+
+# while True:
+#     a, b = map(int, input().split())
+
+#     if a == 0 and b == 0:
+#         break
+
+#     if a > b:
+#         print('Yes')
+#     elif a == b:
+#         print('No')
+#     else:
+#         print('No')
+
+# 2490
+# for i in range(3):
+#     a, b, c, d = map(int, input().split())
+#     sums = a+b+c+d
+#     if sums == 3:
+#         print('A')
+
+#     elif sums == 2:
+#         print('B')
+    
+#     elif sums == 1:
+#         print('C')
+
+#     elif sums == 0:
+#         print('D')
+    
+#     else:
+#         print('E')
+
+# 11721
+
+# N = input()
+
+# for i in range(0, len(N), 10):
+#     print(N[i : i + 10])
