3f1008101c4d3805b9a3f14369ce10049c271569
owinhun <gnsgus190@naver.com>
2022-03-04 10:19:21 +0900

    pk0304

---
+++ GitHub/pk0304.py
@@ -0,0 +1,37 @@
+# 1152
+# N = input().split()
+
+# print(len(N))
+
+# 15596
+
+# def solve(a):
+#    return sum(a)
+
+# 2440
+# N = int(input())
+
+# for i in range(N):
+#     for x in range(N-i):
+#         print('*', end="")
+#     for y in range(i):
+#         print('', end="")
+#     print('')
+
+# 11004
+# a, b = map(int, input().split())
+# nums = list(map(int, input().split()))
+
+# nums.sort()
+# print(nums[b-1])
+
+# 2744
+# N = input()
+
+# for i in N:
+#     if i.islower():
+#         print(i.upper(), end="")
+
+#     else:
+#         print(i.lower(), end="")
+
