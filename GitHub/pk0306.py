96160f48ac1bb29dcf1915ccc3f334769da4f599
owinhun <gnsgus190@naver.com>
2022-03-06 09:39:56 +0900

    pk0306

---
+++ GitHub/pk0306.py
@@ -0,0 +1,11 @@
+# 11053
+# A = int(input())
+# nums = list(map(int, input().split()))
+
+# l1 = [1]*A
+# for i in range(1,A):
+#     for j in range(i):
+#         if nums[j] < nums[i]:
+#             l1[i] = max(l1[i], l1[j] + 1)
+
+# print(max(l1))
