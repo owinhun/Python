8527fd66179cf46bd5099ea917dd813acf782874
owinhun <gnsgus190@naver.com>
2022-03-08 14:34:45 +0900

    pk0308

---
+++ GitHub/pk0308.py
@@ -0,0 +1,20 @@
+# 1789
+
+# S = int(input())
+
+# n = 1
+# while n * (n + 1) / 2 <= S:
+#     n += 1
+# print(n-1)
+
+# 2920
+# nums = list(map(int, input().split()))
+
+# if nums == sorted(nums):
+#     print('ascending')
+
+# elif nums == sorted(nums, reverse=True):
+#     print('descending')
+
+# else:
+#     print('mixed')
