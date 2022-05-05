9c359aaea34aba97975d717d7f98a15729676c09
Seung_Hun Oh (owinhun) <97443033+owinhun@users.noreply.github.com>
2022-05-05 22:44:26 +0900

    Add files via upload

---
+++ GitHub/pk0428.py
@@ -0,0 +1,15 @@
+# 11050
+
+# import math
+
+# a, b = map(int, input().split())
+# print(math.comb(a,b))
+
+# 11051
+
+# import math
+
+# a, b = map(int, input().split())
+# result = math.comb(a,b)
+# newResult = result % 10007
+# print(newResult)
